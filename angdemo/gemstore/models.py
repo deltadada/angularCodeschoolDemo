f0159a06e2f2fe6def72d4b35a9c06ee071d3400
Jeremy Markowitz <jeremyjmarkowitz@gmail.com>
2016-02-17 09:16:04 -0500

    updated column names in model

--- angdemo/gemstore/models.py
+++ angdemo/gemstore/models.py
@@ -12,7 +12,7 @@ IMAGE_TYPES = (
 
 # Create your models here.
 class Gems(models.Model):
-	gems_id = models.AutoField(primary_key=True, blank=False, null=False)
+	id = models.AutoField(primary_key=True, blank=False, null=False)
 	name = models.CharField(max_length = 255, blank=False, null=False )
 	price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, default="0.00")
 	description = models.CharField(max_length = 1056, blank=True, null=True )
@@ -23,18 +23,18 @@ class Gems(models.Model):
 		return self.filter(canPurchase=True, soldOut=False)
 
 class Reviews(models.Model):
-	reviews_id = models.AutoField(primary_key=True, blank=False, null=False)
+	id = models.AutoField(primary_key=True, blank=False, null=False)
 	gem = models.ForeignKey(Gems, on_delete=models.CASCADE, blank=True, null=False) #one Gem to many Reviews
 	stars = models.SmallIntegerField(blank=False, null=False, default=1)
 	body = models.CharField(max_length=800, blank=False, null=False, default="...")
 	author = models.CharField(max_length=254, blank=False, null=False, default="george@internet.com")
 
 class GemImgTypes(models.Model):
-	gemImgTypes_id = models.AutoField(primary_key=True, blank=False, null=False)
+	id = models.AutoField(primary_key=True, blank=False, null=False)
 	imgType = models.CharField(max_length=32, blank=False, null=False, unique=True)
 
 class GemImgs(models.Model):
-	gemImgs_id = models.AutoField(primary_key=True, blank=False, null=False)
+	id = models.AutoField(primary_key=True, blank=False, null=False)
 	gem = models.ForeignKey(Gems, on_delete=models.CASCADE, blank=True, null=False) #one Gem to many  GemImgs
 	imgType = models.ForeignKey(GemImgTypes, on_delete=models.PROTECT, null=True) #one GemImgType to many  GemImgs
 	url = models.URLField(max_length = 2000, blank=False, null=False, default="image_not_avail.png" )
